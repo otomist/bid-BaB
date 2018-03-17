b8f6bba91442541493361ddd690dd5569a8f595c
abochnak <abochnak@umass.edu>
2018-03-17 04:09:46 -0400

    update
    update version of the website

--- airbnb/client/main.js
+++ airbnb/client/main.js
@@ -20,5 +20,3 @@ Template.hello.events({
     instance.counter.set(instance.counter.get() + 1);
   },
 });
-
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
