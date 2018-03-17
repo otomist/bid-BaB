d4c12be7a8be694e387ef09420ce0d614f3b6d64
abochnak <abochnak@umass.edu>
2018-03-17 07:52:00 -0400

    update

--- airbnb/client/function.js
+++ airbnb/client/function.js
@@ -1,3 +1,8 @@
-$(document).ready(function(){
-  alert('asd');
+
+$('#right-jumbotron-arrow').click(function() {
+  var currentSlide = $('.slide.active');
+  var nextSlide = currentSlide.next();
+
+  currentSlide.fadeOut(300).removeClass('active');
+  nextSlide.fadeIn(300).addClass('active');
 });
--- airbnb/client/main.js
+++ airbnb/client/main.js
@@ -2,7 +2,6 @@ import { Template } from 'meteor/templating';
 import { ReactiveVar } from 'meteor/reactive-var';
 
 import './main.html';
-
 Template.hello.onCreated(function helloOnCreated() {
   // counter starts at 0
   this.counter = new ReactiveVar(0);
